e435f7c6cd7523d6b2cc9346ce7776b0ec1e17ab
Mikhail Kopylov <saram139@mail.ru>
2025-01-02 14:06:48 +0300

    1.0

--- module_14_3.py
+++ module_14_3.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = "7851693435:AAHI_dkc9i4cNXXKMyucePbMUAwwowdKrds"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
